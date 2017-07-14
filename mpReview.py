f1ff51a8c9a09271da2ce8cce6c3f0ea045505e7
Christian Herz <christianherz1985@googlemail.com>
2017-07-14 15:32:47 -0400

    ENH: adding python resources automatically to project
    * reusing icons from SlicerDevelopmentToolbox

--- mpReview.py
+++ mpReview.py
@@ -9,13 +9,17 @@ import SimpleITK as sitk
 import sitkUtils
 import datetime
 from slicer.ScriptedLoadableModule import *
+
+from mpReviewPreprocessor import mpReviewPreprocessorLogic
+
+from qSlicerMultiVolumeExplorerModuleWidget import qSlicerMultiVolumeExplorerSimplifiedModuleWidget
+from qSlicerMultiVolumeExplorerModuleHelper import qSlicerMultiVolumeExplorerModuleHelper as MVHelper
+
 from SlicerDevelopmentToolboxUtils.mixins import ModuleWidgetMixin, ModuleLogicMixin
 from SlicerDevelopmentToolboxUtils.buttons import WindowLevelEffectsButton
 from SlicerDevelopmentToolboxUtils.helpers import WatchBoxAttribute
 from SlicerDevelopmentToolboxUtils.widgets import TargetCreationWidget, XMLBasedInformationWatchBox
-from mpReviewPreprocessor import mpReviewPreprocessorLogic
-from qSlicerMultiVolumeExplorerModuleWidget import qSlicerMultiVolumeExplorerSimplifiedModuleWidget
-from qSlicerMultiVolumeExplorerModuleHelper import qSlicerMultiVolumeExplorerModuleHelper as MVHelper
+from SlicerDevelopmentToolboxUtils.icons import Icons
 
 
 class mpReview(ScriptedLoadableModule, ModuleWidgetMixin):
@@ -219,9 +223,7 @@ class mpReviewWidget(ScriptedLoadableModuleWidget, ModuleWidgetMixin):
     self.setupStudySelectionView()
 
   def createHelperLabel(self, toolTipText=""):
-    helperPixmap = qt.QPixmap(os.path.join(self.resourcesPath, 'Icons', 'icon-infoBox.png'))
-    helperPixmap = helperPixmap.scaled(qt.QSize(23, 20))
-    label = self.createLabel("", pixmap=helperPixmap, toolTip=toolTipText)
+    label = self.createLabel("", pixmap=Icons.info.pixmap(qt.QSize(23, 20)), toolTip=toolTipText)
     label.setCursor(qt.Qt.PointingHandCursor)
     return label
 
